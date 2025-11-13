5077d503032c0d30fed1cb8c7f6b6abaaf2074af
Valenti Gamez <vgr.gamez@gmail.com>
2025-11-13 21:47:56 +0100

    SN-3 Added vue quill editor to home.

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -9,6 +9,8 @@ import LoginView from './pages/LoginView.vue'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { QuillEditor } from '@vueup/vue-quill'
+import '@vueup/vue-quill/dist/vue-quill.snow.css';
 import App from './App.vue'
 
 const routes = [
@@ -32,5 +34,8 @@ const api = axios.create({
 });
 // Configuración global de Axios
 app.config.globalProperties.$axios = api;
+
+// Register global components
+app.component('QuillEditor', QuillEditor);
 
 app.mount('#app');
--- src/renderer/src/pages/HomeView.vue
+++ src/renderer/src/pages/HomeView.vue
@@ -1,22 +1,15 @@
 <template>
   <div>
-    Home
+    <QuillEditor theme="snow" />
   </div>
 </template>
 
 <script setup>
 import { onMounted } from 'vue'
-import { useRouter } from 'vue-router'
 import { useUserStore } from '../store/user'
-import { useSettingsStore } from '../store/settings'
-import { useServerMessageStore } from '../store/serverMessage'
 
 // stores
 const user = useUserStore()
-const settings = useSettingsStore()
-
-// router and axios
-const router = useRouter()
 
 function setUserCookies() {
   user.token = localStorage.getItem('token')
@@ -25,9 +18,5 @@ function setUserCookies() {
 
 onMounted(() => {
   setUserCookies()
-  /*if (!user.token) {
-    setTimeout(() => router.push({ path: '/login' }), 1000)
-    return
-  }*/
 })
 </script>
